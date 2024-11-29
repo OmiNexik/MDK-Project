16b680a7779189daa1aa2ebc2935821eff377bdc
anq308 <1angelovyaroslav@gmail.com>
2024-11-29 14:36:33 +0300

    main page html and css

--- index.php
+++ index.php
@@ -62,7 +62,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['action'])) {
 
             if ($user && password_verify($password, $user['password'])) {
                 $_SESSION['user'] = $user['name'];
-                header('Location: welcome.php');
+                header('Location: index.html');
                 exit;
             } else {
                 $message = 'Неверный Email или пароль.';
